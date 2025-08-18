4ce414f1c92c62581db2b36570bebbd117dc005b
Patryk Bandyra <patryk.bandyra@gmail.com>
2025-08-19 00:10:18 +0200

    run training

--- lab/nn/run_training_linux.sh
+++ lab/nn/run_training_linux.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Run training.py with different splits on Linux in parallel
-python3 training.py --os linux --split 0.001 0.001 0.998 --model-path model_split_0001_0001_0998.pth &
-python3 training.py --os linux --split 0.01 0.01 0.98 --model-path model_split_001_001_098.pth &
-python3 training.py --os linux --split 0.1 0.1 0.8 --model-path model_split_01_01_08.pth &
-wait
+#python3 training.py --os linux --split 0.001 0.001 0.998 --model-path model_split_0001_0001_0998.pth &
+#python3 training.py --os linux --split 0.01 0.01 0.98 --model-path model_split_001_001_098.pth &
+#python3 training.py --os linux --split 0.1 0.1 0.8 --model-path model_split_01_01_08.pth &
+#
+#python3 training.py --os linux --split 0.2 0.2 0.6 --model-path model_split_02_02_06.pth &
+#python3 training.py --os linux --split 0.4 0.4 0.2 --model-path model_split_04_04_02.pth &
+#python3 training.py --os linux --split 0.8 0.2 0.0 --model-path model_split_08_02_00.pth &
+#wait
--- lab/nn/run_training_windows.bat
+++ lab/nn/run_training_windows.bat
@@ -3,3 +3,6 @@ REM Run training.py with different splits on Windows in parallel
 start python training.py --os windows --split 0.001 0.001 0.998 --model-path model_split_0001_0001_0998.pth
 start python training.py --os windows --split 0.01 0.01 0.98 --model-path model_split_001_001_098.pth
 start python training.py --os windows --split 0.1 0.1 0.8 --model-path model_split_01_01_08.pth
+start python training.py --os windows --split 0.2 0.2 0.6 --model-path model_split_02_02_06.pth
+start python training.py --os windows --split 0.4 0.4 0.2 --model-path model_split_04_04_02.pth
+start python training.py --os windows --split 0.8 0.2 0.0 --model-path model_split_08_02_00.pth
